7c9324a5e2e57486bebad8a0ad8600f85631f15a
Nellyeyy <80837378+Nellyeyy@users.noreply.github.com>
2021-10-21 14:36:23 +0200

    L01überarbeitet

--- L01/L01.js
+++ L01/L01.js
@@ -1,42 +1,45 @@
 "use strict";
-// Arrays 
-var Subjekt = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dambeldor"];
-var Praedikat = ["liebt", "fliegt", "braut", "zaubert", "zerstört", "hasst"];
-var Objekt = ["Zaubertränke", "einen Dementor", "Hogwarts", "den Stein der Weisen", "die Karte des Rumtreibers", "Malfoy"];
-// Prüfung der Arrays in Console
-// console.log(Subjekt);
-// console.log(Praedikat);
-// console.log(Objekt);
-// for-Schleife - Konsolenausgabe der Sätze
-for (var i = Subjekt.length; i >= 2; i--) { }
-;
-// console.log(i);
-console.log(getVerse(Subjekt, Praedikat, Objekt));
-// function getVerse - Erster Versuch
-// window.onload = function getVerse() {
-//     console.log(Subjekt[1], Praedikat[1], Objekt[1]);
-//     console.log("Alohomora");
-//     console.log(Subjekt);
-//     console.log(Praedikat);
-//     console.log(Objekt);
-//     console.log (i = Subjekt[1]);
-// } 
-// funtion getVerse - Finaler  Versuch / Version
-function getVerse(_Subjekt, _Praedikat, _Objekt) {
-    // Variable String, welche Satz später aufnimmt
-    var satz = "";
-    // Subjekt 
-    let zufallSubjekt = Math.floor(Math.random() * _Subjekt.length);
-    let splicedSubjekt = _Subjekt.splice(zufallSubjekt, 1)[0];
-    satz = satz + " " + splicedSubjekt;
-    // Prädikat
-    let zufallPreadikat = Math.floor(Math.random() * _Praedikat.length);
-    let splicedPraedikat = _Praedikat.splice(zufallPreadikat, 1)[0];
-    satz = satz + " " + splicedPraedikat;
-    // Objekt
-    let zufallObjekt = Math.floor(Math.random() * _Objekt.length);
-    let splicedObjekt = _Objekt.splice(zufallObjekt, 1)[0];
-    satz = satz + " " + splicedObjekt;
-    return satz;
-}
+var RandomPoem;
+(function (RandomPoem) {
+    // Arrays 
+    let subject = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dambeldor"];
+    let verb = ["liebt", "fliegt", "braut", "zaubert", "zerstört", "hasst"];
+    let object = ["Zaubertränke", "einen Dementor", "Hogwarts", "den Stein der Weisen", "die Karte des Rumtreibers", "Malfoy"];
+    // Prüfung der Arrays in Console
+    // console.log(subject);
+    // console.log(verb);
+    // console.log(object);
+    // for-Schleife - Konsolenausgabe der Sätze
+    for (let i = subject.length; i >= 1; i--) {
+        // console.log(i);
+        console.log(getVerse(subject, verb, object));
+    }
+    // function getVerse - Erster Versuch
+    // window.onload = function getVerse() {
+    //     console.log(subject[1], verb[1], object[1]);
+    //     console.log("Alohomora");
+    //     console.log(subject);
+    //     console.log(verb);
+    //     console.log(object);
+    //     console.log (i = subject[1]);
+    // } 
+    // funtion getVerse - Finaler  Versuch / Version
+    function getVerse(_subject, _verb, _object) {
+        // Variable String, welche verse später aufnimmt
+        let verse = "";
+        // subject 
+        let zufallsubject = Math.floor(Math.random() * _subject.length);
+        let splicedsubject = _subject.splice(zufallsubject, 1)[0];
+        verse = verse + " " + splicedsubject;
+        // Prädikat
+        let zufallPreadikat = Math.floor(Math.random() * _verb.length);
+        let splicedverb = _verb.splice(zufallPreadikat, 1)[0];
+        verse = verse + " " + splicedverb;
+        // object
+        let zufallobject = Math.floor(Math.random() * _object.length);
+        let splicedobject = _object.splice(zufallobject, 1)[0];
+        verse = verse + " " + splicedobject;
+        return verse;
+    }
+})(RandomPoem || (RandomPoem = {}));
 //# sourceMappingURL=L01.js.map
--- L01/L01.ts
+++ L01/L01.ts
@@ -1,50 +1,53 @@
+namespace RandomPoem {
 // Arrays 
-    var Subjekt: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape","Dambeldor"];
-    var Praedikat: string[] = ["liebt", "fliegt", "braut", "zaubert", "zerstört","hasst"];
-    var Objekt: string[] = ["Zaubertränke", "einen Dementor", "Hogwarts", "den Stein der Weisen", "die Karte des Rumtreibers","Malfoy"];
+    let subject: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dambeldor"];
+    let verb: string[] = ["liebt", "fliegt", "braut", "zaubert", "zerstört", "hasst"];
+    let object: string[] = ["Zaubertränke", "einen Dementor", "Hogwarts", "den Stein der Weisen", "die Karte des Rumtreibers", "Malfoy"];
     
 // Prüfung der Arrays in Console
-    // console.log(Subjekt);
-    // console.log(Praedikat);
-    // console.log(Objekt);
+    // console.log(subject);
+    // console.log(verb);
+    // console.log(object);
 
 // for-Schleife - Konsolenausgabe der Sätze
-    for (var i = Subjekt.length; i >= 2; i--){};
-    // console.log(i);
-    console.log(getVerse(Subjekt, Praedikat,Objekt));
 
+    for (let i = subject.length; i >= 1; i--) {
+    // console.log(i);
+    console.log(getVerse(subject, verb, object));
+    } 
 
 
 // function getVerse - Erster Versuch
     // window.onload = function getVerse() {
-    //     console.log(Subjekt[1], Praedikat[1], Objekt[1]);
+    //     console.log(subject[1], verb[1], object[1]);
     //     console.log("Alohomora");
-    //     console.log(Subjekt);
-    //     console.log(Praedikat);
-    //     console.log(Objekt);
-    //     console.log (i = Subjekt[1]);
+    //     console.log(subject);
+    //     console.log(verb);
+    //     console.log(object);
+    //     console.log (i = subject[1]);
     // } 
     
 
 // funtion getVerse - Finaler  Versuch / Version
-    function getVerse(_Subjekt: string[], _Praedikat: string[], _Objekt: string[]): string {
-        // Variable String, welche Satz später aufnimmt
-        var satz: string = "";
+    function getVerse(_subject: string[], _verb: string[], _object: string[]): string {
+        // Variable String, welche verse später aufnimmt
+        let verse: string = "";
     
-        // Subjekt 
-        let zufallSubjekt: number = Math.floor(Math.random() * _Subjekt.length);
-        let splicedSubjekt: string = _Subjekt.splice(zufallSubjekt, 1)[0];
-        satz = satz + " " + splicedSubjekt;
+        // subject 
+        let zufallsubject: number = Math.floor(Math.random() * _subject.length);
+        let splicedsubject: string = _subject.splice(zufallsubject, 1)[0];
+        verse = verse + " " + splicedsubject;
     
         // Prädikat
-        let zufallPreadikat: number = Math.floor(Math.random() * _Praedikat.length);
-        let splicedPraedikat: string = _Praedikat.splice(zufallPreadikat, 1)[0];
-        satz = satz + " " + splicedPraedikat;
+        let zufallPreadikat: number = Math.floor(Math.random() * _verb.length);
+        let splicedverb: string = _verb.splice(zufallPreadikat, 1)[0];
+        verse = verse + " " + splicedverb;
     
-        // Objekt
-        let zufallObjekt: number = Math.floor(Math.random() * _Objekt.length);
-        let splicedObjekt: string = _Objekt.splice(zufallObjekt, 1)[0];
-        satz = satz + " " + splicedObjekt;
+        // object
+        let zufallobject: number = Math.floor(Math.random() * _object.length);
+        let splicedobject: string = _object.splice(zufallobject, 1)[0];
+        verse = verse + " " + splicedobject;
     
-        return satz;
+        return verse;
     }
+}
