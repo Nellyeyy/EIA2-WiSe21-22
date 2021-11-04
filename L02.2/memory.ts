81dee54d972b780b91f3fa86649bf50b40ba6b98
Nellyeyy <80837378+Nellyeyy@users.noreply.github.com>
2021-11-04 21:26:08 +0100

    L02.2 weiter

--- L02.2/memory.ts
+++ L02.2/memory.ts
@@ -1,16 +1,38 @@
-namespace SequenMemory {
+namespace sequenzMemory {
 
-    window.addEventListener("load", handload);
+    let sequenz: string = "A";
 
-    function handload () {
+    window.addEventListener("load", gameload);
 
-    };
+    function gameload (_event: Event): void {
+        let game: HTMLElement = <HTMLElement>document.querySelector("div#gameboard");
+        game.addEventListener("click", placeLetter);
+        document.addEventListener("keydown", choosesequenz);
+    }
 
+    function placeLetter (_event: MouseEvent): void {
+        let x: number = _event.offsetX;
+        let y: number = _event.offsetY;
 
+        let game: HTMLElement = <HTMLElement>_event.target;
+        let span: HTMLSpanElement = document.createElement("span");
+        
+        game.appendChild(span);
 
+        span.textContent = sequenz;
+        span.style.left = x + "px";
+        span.style.top = y + "px";
 
+        span.addEventListener("click", deleteLetter);
+    }
 
+    function choosesequenz (_event: KeyboardEvent): void {
+        sequenz = _event.key;
+    }
 
-
-
+    function deleteLetter (_event: MouseEvent): void {
+        let target: Node = <Node>_event.target;
+        let parent: Node = <Node>target.parentNode;
+        parent.removeChild(target);
+    }
 }
