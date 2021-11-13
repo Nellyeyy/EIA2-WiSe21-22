a87ccacc69a53fd1821f78c589fff701bf57946c
Nellyeyy <80837378+Nellyeyy@users.noreply.github.com>
2021-11-13 21:40:06 +0100

    L3

--- L02.2/memory.js
+++ L02.2/memory.js
@@ -1,9 +1,31 @@
 "use strict";
-var SequenMemory;
-(function (SequenMemory) {
-    window.addEventListener("load", handload);
-    function handload() {
+var sequenzMemory;
+(function (sequenzMemory) {
+    let sequenz = "A";
+    window.addEventListener("load", gameload);
+    function gameload(_event) {
+        let game = document.querySelector("div#gameboard");
+        game.addEventListener("click", placeLetter);
+        document.addEventListener("keydown", choosesequenz);
     }
-    ;
-})(SequenMemory || (SequenMemory = {}));
+    function placeLetter(_event) {
+        let x = _event.offsetX;
+        let y = _event.offsetY;
+        let game = _event.target;
+        let span = document.createElement("span");
+        game.appendChild(span);
+        span.textContent = sequenz;
+        span.style.left = x + "px";
+        span.style.top = y + "px";
+        span.addEventListener("click", deleteLetter);
+    }
+    function choosesequenz(_event) {
+        sequenz = _event.key;
+    }
+    function deleteLetter(_event) {
+        let target = _event.target;
+        let parent = target.parentNode;
+        parent.removeChild(target);
+    }
+})(sequenzMemory || (sequenzMemory = {}));
 //# sourceMappingURL=memory.js.map
